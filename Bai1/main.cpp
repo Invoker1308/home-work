b3452022231ae40c01b07c400e43944e5335b9d8
Invoker1308 <binhbnvm@gmail.com>
2024-08-14 22:51:04 +0700

    command, add note for the table

--- Bai1/main.cpp
+++ Bai1/main.cpp
@@ -13,9 +13,11 @@ class Employee{
         int Age;
         string Sex;
         string Address;
+        int Id;
 
     public:
 
+        //set name for employee
         void setName(){
             cout << "Name: ";
             cin.get();
@@ -26,6 +28,7 @@ class Employee{
             return Name;
         }
 
+        //set age for employee
         void setAge(){
             cout << "Age: ";
             cin >> Age;
@@ -35,6 +38,7 @@ class Employee{
             return Age;
         }
 
+        //set sex for employee
         void setSex(){
             cout << "Sex: ";
             cin.get();
@@ -45,15 +49,31 @@ class Employee{
             return Sex;
         }
 
+        //set address for employee
         void setAddress(){
             cout << "Address: ";
-            cin.get();
             getline(cin,Address);
         }
 
         string getAddress(){
             return Address;
         }
+
+        //set id for employee: 1 = worker, 2 = engineer, 3 = staff
+        void setId(int x){
+            Id = x;
+        }
+
+        int getId(){
+            return Id;
+        }
+
+        virtual void setLevel(){}
+        virtual int getLevel(){}
+        virtual void setMajor(){}
+        virtual string getMajor(){}
+        virtual void setJob(){}
+        virtual string getJob(){}
 
 };
 
@@ -65,7 +85,9 @@ class Worker : public Employee{
 
     public:
 
+        //set level for worker
         void setLevel(){
+            cout << "Level: ";
             cin >> Level;
         }
 
@@ -76,16 +98,16 @@ class Worker : public Employee{
 };
 
 class Engineer : public Employee{
-    //
+
     private:
 
         string Major;
 
     public:
 
+        //set major for engineer
         void setMajor(){
             cout << "Major: ";
-            cin.get();
             getline(cin,Major);
         }
 
@@ -103,9 +125,9 @@ class Staff : public Employee{
 
     public:
 
+        //set job for staff
         void setJob(){
             cout << "Job: ";
-            cin.get();
             getline(cin,Job);
         }
 
@@ -119,46 +141,70 @@ class EmployeeManagemnt{
 
     public:
 
+        // add new employee to vector
         void AddEmployee(vector<Employee*> &v, int p){
+            //set worker information
             if (p == 1){
                 v.push_back(new Worker);
+                v[v.size()-1]->setId(1);
                 v[v.size()-1]->setName();
                 v[v.size()-1]->setAge();
                 v[v.size()-1]->setSex();
                 v[v.size()-1]->setAddress();
+                v[v.size()-1]->setLevel();
             }
+            //set engineer information
             if (p == 2){
                 v.push_back(new Engineer);
+                v[v.size()-1]->setId(2);
                 v[v.size()-1]->setName();
                 v[v.size()-1]->setAge();
                 v[v.size()-1]->setSex();
                 v[v.size()-1]->setAddress();
+                v[v.size()-1]->setMajor();
             }
+            //set staff information
             if (p == 3){
                 v.push_back(new Staff);
+                v[v.size()-1]->setId(3);
                 v[v.size()-1]->setName();
                 v[v.size()-1]->setAge();
                 v[v.size()-1]->setSex();
                 v[v.size()-1]->setAddress();
+                v[v.size()-1]->setJob();
             }
         }
 
+        //search employee in vector
         void SearchEmployee(vector<Employee*> v){
             string s;
             cout << "Enter the name you want to search for: ";
             cin >> s;
-            cout << left << setw(20) << "Name" << setw(5) << "Age"  << setw(10) << "Sex" << setw(30) << "Address" << endl;
+            cout << left << setw(8) << "Number" << setw(20) << "Name" << setw(5) << "Age"  << setw(10) << "Sex" << setw(30) << "Address" << setw(20) << "Note" << endl;
             for (int i=0;i<v.size();i++){
                 if (strstr(v[i]->getName().c_str(),s.c_str()) != nullptr){
-                    cout << left << setw(20) << v[i]->getName() << setw(5) << v[i]->getAge()  << setw(10) << v[i]->getSex() << setw(30) << v[i]->getAddress() << endl;
+                    if (v[i]->getId() == 1)
+                        cout << left << setw(8) << i+1 << setw(20) << v[i]->getName() << setw(5) << v[i]->getAge()  << setw(10) << v[i]->getSex() << setw(30) << v[i]->getAddress() << setw(7) << "Level: " << v[i]->getLevel() << endl;
+                    if (v[i]->getId() == 2)
+                        cout << left << setw(8) << i+1 << setw(20) << v[i]->getName() << setw(5) << v[i]->getAge()  << setw(10) << v[i]->getSex() << setw(30) << v[i]->getAddress() << setw(7) << "Major: " << v[i]->getMajor() << endl;
+                    if (v[i]->getId() == 3)
+                        cout << left << setw(8) << i+1 << setw(20) << v[i]->getName() << setw(5) << v[i]->getAge()  << setw(10) << v[i]->getSex() << setw(30) << v[i]->getAddress() << setw(5) << "Job: " << v[i]->getJob() << endl;
                 }
             }
         }
 
+        //display the list of employee
         void DisplayList(vector<Employee*> v){
-            cout << left << setw(20) << "Name" << setw(5) << "Age"  << setw(10) << "Sex" << setw(30) << "Address" << endl;
-            for (int i=0;i<v.size();i++)
-                cout << left << setw(20) << v[i]->getName() << setw(5) << v[i]->getAge()  << setw(10) << v[i]->getSex() << setw(30) << v[i]->getAddress() << endl;
+            cout << left << setw(8) << "Number" << setw(20) << "Name" << setw(5) << "Age"  << setw(10) << "Sex" << setw(30) << "Address" << setw(20) << "Note" << endl;
+            for (int i=0;i<v.size();i++){
+                if (v[i]->getId() == 1)
+                    cout << left << setw(8) << i+1 << setw(20) << v[i]->getName() << setw(5) << v[i]->getAge()  << setw(10) << v[i]->getSex() << setw(30) << v[i]->getAddress() << setw(7) << "Level: " << v[i]->getLevel() << endl;
+                if (v[i]->getId() == 2)
+                    cout << left << setw(8) << i+1 << setw(20) << v[i]->getName() << setw(5) << v[i]->getAge()  << setw(10) << v[i]->getSex() << setw(30) << v[i]->getAddress() << setw(7) << "Major: " << v[i]->getMajor() << endl;
+                if (v[i]->getId() == 3)
+                    cout << left << setw(8) << i+1 << setw(20) << v[i]->getName() << setw(5) << v[i]->getAge()  << setw(10) << v[i]->getSex() << setw(30) << v[i]->getAddress() << setw(5) << "Job: " << v[i]->getJob() << endl;
+            }
+
         }
 
 };
@@ -166,9 +212,13 @@ class EmployeeManagemnt{
 vector<Employee*> Data;
 
 int main() {
+    //option 1->4: 4 features
+    //position 1->3: 1 = worker, 2 = engineer, 3 = staff
     int option,position;
     EmployeeManagemnt EM;
+    //loop until the user exits
     while(1){
+        //display 4 options
         cout << "----------------------------------------------------------------------------------------------------" << endl;
         cout << "Choose a feature:" << endl;
         cout << "1.Add a new employee" << endl;
@@ -183,7 +233,7 @@ int main() {
             cin >> option;
         }
 
-
+        //option 1
         if (option == 1){
             cout << "Choose a job position:" << endl;
             cout << "1.Worker" << endl;
@@ -199,17 +249,17 @@ int main() {
             EM.AddEmployee(Data,position);
         }
 
-
+        //option 2
         if (option == 2){
             EM.SearchEmployee(Data);
         }
 
-
+        //option 3
         if (option == 3){
             EM.DisplayList(Data);
         }
 
-
+        //option 4
         if (option == 4){
             break;
         }
